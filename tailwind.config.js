097e11325f43644cd99ac7272e6c7c1e2aa83a65
Ashton Bowens <ashxgrey11@gmail.com>
2023-03-14 17:15:10 -0700

    debugged tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  purge: {
+    content: ["./src/**/*.{js,jsx}", "./index.html"],
   theme: {
     
     colors: {
@@ -10,6 +11,8 @@ module.exports = {
       'dark': '#000000',
     },
   },
+  },
+  
   extend: {
     fontFamily: {
       'quicksand': 'Quicksand'
